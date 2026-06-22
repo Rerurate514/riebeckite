db4a1910eb0ae0d7d3f70445ba7d6b206ae63419
Rerurate514 <rerurateyuto@gmail.com>
2026-06-22 17:33:22 +0900

    frontmatter処理を追加

--- packages/core/src/pipeline.ts
+++ packages/core/src/pipeline.ts
@@ -5,6 +5,8 @@ import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 import remarkDirective from "remark-directive";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkExtractFrontmatter from "remark-extract-frontmatter";
+import { parse as parseYaml } from "yaml";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
@@ -13,10 +15,11 @@ import { unified } from "unified";
 
 export class Pipeline {
   async execute(markDownContent: string) {
-    const result = await unified()
+    const file = await unified()
       .use(remarkParse)
       .use(remarkDirective)
-      .use(remarkFrontmatter)
+      .use(remarkFrontmatter, ["yaml"])
+      .use(remarkExtractFrontmatter, { yaml: parseYaml, remove: true })
       .use(remarkMath)
       .use(remarkGfm)
       .use(remarkRehype, { allowDangerousHtml: true })
@@ -25,8 +28,9 @@ export class Pipeline {
       .use(rehypeSanitize)
       .use(rehypeKatex)
       .use(rehypeStringify)
-      .process(markDownContent);
+      .process(markDownContent.trim());
 
-    console.log(result.value);
+    console.log(file.value);
+    console.log(file.data);
   }
 }
